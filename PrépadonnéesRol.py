97c6af56e20a12418808ab9770659a04611da4d0
« rolandM91 » <rolandmaury05@gmail.com>
2025-12-06 16:55:57 +0100

    suppresion des NA

--- PrépadonnéesRol.py
+++ PrépadonnéesRol.py
@@ -1,1 +1,8 @@
-df = 2
+import pandas as pd
+
+data = "/Users/roland/Desktop/ENSAE 2A/Python data/projet_python_roland_sacha/data/raw/athlete_events.csv"
+df_athlete = pd.read_csv(data)
+df_athlete = df_athlete.dropna(subset = "Medal")
+df_athlete
+
+
