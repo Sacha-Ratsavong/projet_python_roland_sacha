392e30f3a60c3923c8be73cc5a59f369641cbdce
« rolandM91 » <rolandmaury05@gmail.com>
2025-12-06 17:13:01 +0100

    Tableau des médailles

--- PrépadonnéesRol.py
+++ PrépadonnéesRol.py
@@ -2,7 +2,11 @@ import pandas as pd
 
 data = "/Users/roland/Desktop/ENSAE 2A/Python data/projet_python_roland_sacha/data/raw/athlete_events.csv"
 df_athlete = pd.read_csv(data)
+#On enlève les athlètes sans médailles
 df_athlete = df_athlete.dropna(subset = "Medal")
-df_athlete
+
+df_medals = medal_counts = df_athlete.groupby(['Year', 'Team', 'Medal']).size().reset_index(name='Count')
+df_medals
+
 
 
