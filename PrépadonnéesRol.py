d6dbf66f97f4e6ee71337921103a85e1437b5d39
MAURY Roland <Roland.MAURY@ensae.fr>
2025-12-08 15:43:23 +0100

    g

--- PrépadonnéesRol.py
+++ PrépadonnéesRol.py
@@ -1,13 +1,21 @@
 import pandas as pd
 
-data = "/Users/roland/Desktop/ENSAE 2A/Python data/projet_python_roland_sacha/data/raw/athlete_events.csv"
+data = "W:/Bureau/Projet Python/Code/projet_python_roland_sacha/data/raw/athlete_events.csv"
 df_athlete = pd.read_csv(data)
 #On enlève les athlètes sans médailles
 df_athlete = df_athlete.dropna(subset = "Medal")
 
 #On a le tableau des médailles / pays / édition
 df_medals = medal_counts = df_athlete.groupby(['Year', 'Team', 'Medal']).size().reset_index(name='Count')
-df_medals
 
 
+
+
+df_2024 = pd.read_csv("data/raw/paris-2024-results-medals-oly-eng.csv", sep = ";", encoding = "utf-8", on_bad_lines = "skip")
+df_2024["Year"] = pd.to_datetime(df_2024["Medal_date"]).dt.year
+medal_counts_2024 = df_2024.groupby(['Year', 'Country', 'Medal_type']).size().reset_index(name='Count')
+medal_counts_2024 = medal_counts_2024.rename(columns={"Medal_type": "Medal", "Country" : "Team"})
+
+df_jeux = pd.concat([df_medals, medal_counts_2024], ignore_index= True )
+df_jeux
 
