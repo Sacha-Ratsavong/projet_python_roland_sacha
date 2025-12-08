3416258345229b29646c7c4773eddfdd1ba86f81
rolandM91 <rolandmaury05@gmail.com>
2025-12-08 16:41:46 +0100

    Tableau des médailles

--- PrépadonnéesRol.py
+++ PrépadonnéesRol.py
@@ -16,6 +16,29 @@ df_2024["Year"] = pd.to_datetime(df_2024["Medal_date"]).dt.year
 medal_counts_2024 = df_2024.groupby(['Year', 'Country', 'Medal_type']).size().reset_index(name='Count')
 medal_counts_2024 = medal_counts_2024.rename(columns={"Medal_type": "Medal", "Country" : "Team"})
 
-df_jeux = pd.concat([df_medals, medal_counts_2024], ignore_index= True )
+df_jeux = pd.concat([df_medals, medal_counts_2024], ignore_index= True)
 df_jeux
+
+df_tokyo = pd.read_csv("data/raw/Tokyo Olympics  2021 dataset.csv", sep = ",", encoding = "utf-8", on_bad_lines = "skip") 
+print(df_tokyo.columns.tolist())
+df_tokyo = df_tokyo.rename(columns={"Gold Medal": "Gold", "Bronze Medal" : "Bronze", "Silver Medal" : "Silver", "Team/NOC" : "Team"})
+df_tokyo["Year"] = 2021
+df_tokyo = df_tokyo.drop(["Total", "Rank by Total", "Rank", "NOCCode"], axis=1)
+
+df_long = df_tokyo.melt(
+    id_vars=["Team", "Year"],      # colonnes qui restent fixes
+    value_vars=["Gold", "Silver", "Bronze"],   # colonnes à transformer
+    var_name="Medal",             # nom de la nouvelle colonne
+    value_name="Count"                # nombre de médailles
+)
+df_long
+
+#On a le df des médailles pr toutes les éditions c bon
+df_all_games = pd.concat([df_jeux, df_long], ignore_index = True)
+df_all_games = df_all_games.sort_values(by="Year")
+df_all_games
+
+
+
+
 
