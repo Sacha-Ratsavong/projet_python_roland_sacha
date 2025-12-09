1ff629f580a6694c90a98695f59f5737ca3b2b4b
« rolandM91 » <rolandmaury05@gmail.com>
2025-12-09 10:52:51 +0100

    pib_hab

--- PrépadonnéesRol.py
+++ PrépadonnéesRol.py
@@ -81,4 +81,29 @@ plt.show()
 
 import pandas as pd
 #PIB/hab et revenus mondiaux
-df_pib = pd.read_csv("data/raw/Data/GDP_hab.csv")
+df_pib = pd.read_csv(
+    "data/raw/Data/GDP_hab.csv",
+    sep=",",
+    encoding="utf-8",
+    skiprows=4   # souvent 3, 4 ou 5 pour les fichiers World Bank
+)
+
+df_pib.head()
+df_pib
+
+jo_years = [2012, 2016, 2020]
+for jo in jo_years:
+    start = jo - 4
+    years = [str(y) for y in range(start, jo)]
+    df_pib[f'PIB_mean_{jo}'] = df_pib[years].mean(axis=1)
+
+df_long = pd.melt(
+    df_pib,
+    id_vars=['Country Name', 'Country Code'],
+    value_vars=[f'PIB_mean_{jo}' for jo in jo_years],
+    var_name='JO Year',
+    value_name='PIB_mean'
+)
+
+# Ajuster la colonne 'JO Year' pour ne garder que l'année
+df_long['JO Year'] = df_long['JO Year'].str.extract('(\d+)$').astype(int)
